879266ffa29bd7ef5e2e705311186244d0fa2a5a
Tadhg N <tadhgnolan@gmail.com>
2021-08-11 08:23:54 +0000

    Add intro message and start game functions

--- run.py
+++ run.py
@@ -6,4 +6,30 @@ def intro_msg():
     """
 
     print()
-    ("##################################\n", 0.01)
+    typing("##################################\n", 0.01)
+    typing("#                                #\n", 0.01)
+    typing("#   The Fall of Roman Britain    #\n", 0.01)
+    typing("##################################\n\n", 0.01)
+
+
+
+def start_game():
+    """
+    Starts game, retrieves username from player & welcomes them to the game.
+    """
+
+    intro_msg()
+
+
+    while True:
+
+        global PLAYER_NAME
+        PLAYER_NAME = input("Please enter a name for your character: \n")
+        print()
+        if PLAYER_NAME = "":
+            print("Please enter a character name to continue...\n")
+            continue
+        else:
+            break
+    typing(f"Salve {PLAYER_NAME}. Alae iacta est! (Greetings {PLAYER_NAME}. The die is cast!\n\n, 0,03")
+    option_one()
